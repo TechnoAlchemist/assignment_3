0bb2caf63a0f5510c2734bc95aa3862cf576c58d
Gabe Lafontant <glafonta@gmail.com>
2015-04-03 14:34:15 -0400

    temporary logic

--- AlarmClock.java
+++ AlarmClock.java
@@ -1,3 +1,32 @@
 public class AlarmClock {
-  
+
+  private int currentHour;
+  private int currentMinute;
+  private int alarmHour;
+  private int alarmMinute;
+  private boolean isAlarmOn;
+
+
+  // constructor 
+  public AlarmClock() {
+    currentHour = 12;
+    currentMinute = 00;
+    alarmHour = 0;
+    alarmMinute = 0;
+    isAlarmOn = false;
+  }
+
+  public int setAlarm(int hour, int minute) {
+    currentHour = hour;
+    currentMinute = minute;
+
+  } 
+
+  public int snooze() {
+
+  }
+
+  public int dismiss() {
+
+  }
 }
